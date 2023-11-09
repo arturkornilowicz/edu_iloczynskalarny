b5ffa39ae3f6dbcf2ff408513c01bfd331757715
Artur Kornilowicz <arturk@math.uwb.edu.pl>
2023-11-09 09:46:05 +0100

    Poprawiona funkcja main

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,22 @@ public class Main {
     public static void main(String[] args) {
         List<LiczbaCalkowita> v1c = List.of(new LiczbaCalkowita(11),new LiczbaCalkowita(3));
         List<LiczbaCalkowita> v2c = List.of(new LiczbaCalkowita(2),new LiczbaCalkowita(-14));
-        IloczynSkalarny iloczynSkalarnyC = new IloczynSkalarny<LiczbaCalkowita>(v1c,v2c,new LiczbaCalkowita().zero());
+        IloczynSkalarny<LiczbaCalkowita> iloczynSkalarnyC = new IloczynSkalarny<>(v1c,v2c,new LiczbaCalkowita().zero());
         System.out.println(iloczynSkalarnyC.iloczyn());
 
         List<LiczbaRzeczywista> v1r = List.of(new LiczbaRzeczywista(11),new LiczbaRzeczywista(3));
         List<LiczbaRzeczywista> v2r = List.of(new LiczbaRzeczywista(2),new LiczbaRzeczywista(-14));
-        IloczynSkalarny iloczynSkalarnyR = new IloczynSkalarny<LiczbaRzeczywista>(v1r,v2r,new LiczbaRzeczywista().zero());
+        IloczynSkalarny<LiczbaRzeczywista> iloczynSkalarnyR = new IloczynSkalarny<>(v1r,v2r,new LiczbaRzeczywista().zero());
         System.out.println(iloczynSkalarnyR.iloczyn());
 
         List<LiczbaWymierna> v1w = List.of(new LiczbaWymierna(11),new LiczbaWymierna(3));
         List<LiczbaWymierna> v2w = List.of(new LiczbaWymierna(2),new LiczbaWymierna(-14));
-        IloczynSkalarny iloczynSkalarnyW = new IloczynSkalarny<LiczbaWymierna>(v1w,v2w,new LiczbaWymierna().zero());
+        IloczynSkalarny<LiczbaWymierna> iloczynSkalarnyW = new IloczynSkalarny<>(v1w,v2w,new LiczbaWymierna().zero());
         System.out.println(iloczynSkalarnyW.iloczyn());
 
         List<LiczbaZespolona> v1z = List.of(new LiczbaZespolona(11),new LiczbaZespolona(3));
         List<LiczbaZespolona> v2z = List.of(new LiczbaZespolona(2),new LiczbaZespolona(-14));
-        IloczynSkalarny iloczynSkalarnyZ = new IloczynSkalarny<LiczbaZespolona>(v1z,v2z,new LiczbaZespolona().zero());
+        IloczynSkalarny<LiczbaZespolona> iloczynSkalarnyZ = new IloczynSkalarny<>(v1z,v2z,new LiczbaZespolona().zero());
         System.out.println(iloczynSkalarnyZ.iloczyn());
     }
 }
